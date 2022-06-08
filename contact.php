867dc956ae02541ac8a00bf7f9b28da391db0c2e
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-06-08 21:46:57 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
 
                 <!-- Contact Form Wrapper Start -->
                 <div class="contact-form-wrapper contact-form">
-                    <form action="" id="contactForm" method="post" class="form-control">
+                    <form action="" id="contactForm" method="post">
                         <div class="row">
                             <div class="col-12">
                                 <div class="row">
@@ -58,8 +58,8 @@
             <div class="col-12 col-lg-6 m-b-50 order-1 order-lg-2" data-aos="fade-up-left" data-aos-duration="1500">
                 <!-- Section Title End -->
                 <!-- Google Map Area Start -->
-                <div class="google-map-area w-100" style="border-radius: 30px">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.841518408663!2d105.76842661447657!3d10.029933692830634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0895a51d60719%3A0x9d76b0035f6d53d0!2zxJDhuqFpIGjhu41jIEPhuqduIFRoxqE!5e0!3m2!1svi!2s!4v1650011390597!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="google-map-area w-100">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.841518408663!2d105.76842661447657!3d10.029933692830634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0895a51d60719%3A0x9d76b0035f6d53d0!2zxJDhuqFpIGjhu41jIEPhuqduIFRoxqE!5e0!3m2!1svi!2s!4v1650011390597!5m2!1svi!2s" width="550" height="400" style="border-radius:10px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <!-- Google Map Area Start -->
             </div>
